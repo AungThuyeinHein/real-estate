9b9a92484c9d9225dd7ec5e003e52d0a8d4aefed
B@C <aungthuyeinhein98@gmail.com>
2024-06-09 21:30:06 +0630

    pushing again

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   updateUser,
   deleteUser,
   getUserListing,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/:id", verifyUser, updateUser);
 router.delete("/:id", verifyUser, deleteUser);
+router.get("/:id", verifyUser, getUser);
 router.get("/listings/:id", verifyUser, getUserListing);
 
 export default router;
