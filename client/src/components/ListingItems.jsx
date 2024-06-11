1569058cd32eb7af4eac2ae9cbface1a353f1476
B@C <aungthuyeinhein98@gmail.com>
2024-06-11 19:39:23 +0630

    create listingitems componemts ans show listing

---
+++ client/src/components/ListingItems.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
+export default function ListingItems({ listing }) {
+  return (
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+      <Link to={`listing/${listing._id}`}>
+        <img
+          src={
+            listing.imageUrls[0] ||
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fkit8.net%2Fillustrations%2Frealestate%2F&psig=AOvVaw0dDjgsiI46hD66hKmK1nce&ust=1718197248018000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjF5trN04YDFQAAAAAdAAAAABAK"
+          }
+          alt="listing cover"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+        />
+        <div className="p-3 flex flex-col gap-2 w-full">
+          <p className="truncate text-lg font-semibold text-slate-700">
+            {listing.name}
+          </p>
+          <div className="flex items-center gap-1">
+            {" "}
+            <MdLocationOn className="h-4 w-4 text-green-700" />
+            <p className="text-sm text-gray-600 truncate w-full">
+              {listing.address}
+            </p>
+          </div>
+          <p className="text-sm text-gray-700 line-clamp-4">
+            {listing.description}
+          </p>
+          <p className="text-slate-500 mt-2 font-semibold ">
+            $
+            {listing.offer
+              ? listing.discountPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
+            {listing.type === "rent" && " /month"}
+          </p>
+          <div className="text-slate-700 flex items-center gap-2">
+            <FaBed className="h-4 w-4 text-green-700" />
+            <div className="font-bold text-xs">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} Bedrooms`
+                : `${listing.bedrooms} Bedroom`}
+            </div>
+            <FaBath className="h-4 w-4 text-green-700" />
+            <div className="font-bold text-xs">
+              {listing.bathtooms > 1
+                ? `${listing.bedrooms} Bathrooms`
+                : `${listing.bedrooms} Bathroom`}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -9,13 +9,11 @@ import {
   FaBath,
   FaBed,
   FaChair,
-  FaMapMarkedAlt,
   FaMapMarkerAlt,
   FaParking,
   FaShare,
 } from "react-icons/fa";
 import Contact from "../components/Contact";
-import { list } from "firebase/storage";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -118,14 +116,14 @@ export default function Listing() {
               <li className="flex items-center gap-1 whitespace-nowrap ">
                 <FaBed className="text-lg" />
                 {listing.bedrooms > 1
-                  ? `${listing.bedrooms} beds `
-                  : `${listing.bedrooms} bed `}
+                  ? `${listing.bedrooms} bedrooms `
+                  : `${listing.bedrooms} bedroom `}
               </li>
               <li className="flex items-center gap-1 whitespace-nowrap ">
                 <FaBath className="text-lg" />
                 {listing.bathrooms > 1
-                  ? `${listing.bathrooms} baths `
-                  : `${listing.bathrooms} bath `}
+                  ? `${listing.bathrooms} bathrooms `
+                  : `${listing.bathrooms} bathroom `}
               </li>
               <li className="flex items-center gap-1 whitespace-nowrap ">
                 <FaParking className="text-lg" />
